76dbb03df291371613873116778ee0d03e1a6d43
TraBao <tratan18111996@gmail.com>
2025-04-22 08:54:03 +0700

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Coffee.Models.Interface;
+using coffeeshop.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.Controllers
+{
+        public class ProductsController : Controller
+        {
+          private readonly IProductRepository _productRepository;
+    
+          public ProductsController(IProductRepository productRepository)
+          {
+                _productRepository = productRepository;
+          }
+    
+          public IActionResult Shop()
+          {
+                return View();
+          }
+        }
+}
---
+++ Models/Interface/IProduceRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Coffee.Models.Interface
+{
+    public interface IProductRepository
+        {
+            IEnumerable<Product> GetAllProducts();
+            IEnumerable<Product> GetTrendingProducts();
+            Product GetProductDetail(int id);
+        }
+}
